c28a6b6b07dcf5ce3be996ebd71d51f2fc8e4e98
RR <85013630+RR-089@users.noreply.github.com>
2023-07-12 01:48:41 +0700

    Update config.js

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "",
+    backendUrl: "https://implementasi-open-aiapi-dalle.vercel.app/api/v1/dalle",
   },
 };
 
